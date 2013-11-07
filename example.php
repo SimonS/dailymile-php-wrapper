823a9a1e10b9737989e30fb0dcd5771a049d71ff
Simon Scarfe <simon.scarfe@gmail.com>
2013-11-07 23:11:26 +0000

    made examples clearer

--- example.php
+++ example.php
@@ -4,4 +4,8 @@ require 'vendor/autoload.php';
 
 $client = new DailymilePHP\Client;
 
+// Get latest public entries
+var_dump($client->getEntries());
+
+// Get latest entries of specific unprotected username
 var_dump($client->getEntries('simons'));
